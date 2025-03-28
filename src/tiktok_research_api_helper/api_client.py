a104a59041b6fe396cfd2bb2b6465a297a621d84
Paul <mountainmanduke@gmail.com>
2025-03-28 15:24:16 -0400

    make UserInfo likes_count, video_count, follower_count, and following_count, as well as Comment video_id, parent_comment_id, like_count, reply_count bigintegers. since they can exceed 32 bit int (#109)
    * make UserInfo likes_count, video_count, follower_count, and following_count, as well as Comment video_id, parent_comment_id, like_count, reply_count bigintegers. since they can exceed 32 bit int
    
    * fix import path in alembic/env.py for https://github.com/CybersecurityForDemocracy/tiktok-library/pull/31
    
    * alembic script to update UserInfo likes_count, video_count, follower_count, and following_count, as well as Comment video_id, parent_comment_id, like_count, reply_count DB schema to bigintegers.
    
    * Linting
    
    ---------
    
    Co-authored-by: BrunoGomesCoelho <bgc5612@nyu.edu>

--- src/tiktok_research_api_helper/api_client.py
+++ src/tiktok_research_api_helper/api_client.py
@@ -975,7 +975,7 @@ class TikTokApiClient:
 
             has_more = response.data.get("has_more", False)
             cursor = response.data.get("cursor")
-            
+
             if cursor is None:
                 logging.debug(
                     "Stopping comments fetch for video ID %s because cursor is empty.", video_id
